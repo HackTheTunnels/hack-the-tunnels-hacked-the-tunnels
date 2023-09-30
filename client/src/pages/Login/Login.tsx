a5239e35da6336df9c70a9f851bef8062381c6bc
Mike Lin <95307563+Maiku3@users.noreply.github.com>
2023-09-30 17:30:22 +0000

    Completed Add Login Page Form #10

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -6,15 +6,17 @@ import "./Login.style.scss";
 
 function Login() {
   const [message, setMessage] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { loggedIn, login } = useAccountContext();
   const navigate = useNavigate();
 
   const attemptLogin = async () => {
     try {
-      const message = await login("admin@email.com", "password");
+      const message = await login(email, password);
       setMessage(message);
     } catch (error) {
-      console.log(error);
+      setMessage(error.message);
     }
   };
 
@@ -28,6 +30,8 @@ function Login() {
     <Page>
       <div className="login-page">
         <h1>Login</h1>
+        <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/><br/><br/>
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/><br/><br/>
         <button onClick={() => attemptLogin()}>
           Login (as user set in code)
         </button>
